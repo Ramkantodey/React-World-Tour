77b21d45dff9efa7401995235a62b7f27049a65a
Ramkantodey <ramkantodey@gmail.com>
2024-10-19 22:36:16 +0600

    React world tour

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,17 +1,39 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
-
+import './countries.css'
 const countries = () => {
     const [countries, setCountries] = useState([])
+    const [visitedContries, setVisitedCountries] = useState([]);
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
             .then(data => setCountries(data))
     }, [])
+
+    const handleVisitedCountry = country => {
+        console.log('add this to you visited country');
+        console.log(country);
+    }
+
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
+            <div>
+                <h5>Visited Countries</h5>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+            <div className="country-container">
+                {
+                    countries.map(country => <Country
+                        key={country.cca3}
+                        handleVisitedCountry={handleVisitedCountry}
+                        country={country}>
+                    </Country>)
+                }
+            </div>
 
         </div>
     );
